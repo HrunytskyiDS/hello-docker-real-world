776a087a42f8fe58f2530c8ede03c933e580563f
HrunytskyiDS <ipzm242_gds@student.ztu.edu.ua>
2024-12-09 15:57:51 +0200

    First commit

--- api/src/routers/user.js
+++ api/src/routers/user.js
@@ -1,25 +1,34 @@
-const express = require("express")
-const User = require("../models/user")
-const router = new express.Router()
+const express = require("express");
+const User = require("../models/user");
+const router = new express.Router();
 
 router.post("/", async (req, res) => {
-    try {
-        const user = new User(req.body)
-        await user.save()
-        res.json(user);
-    } catch (error) {
-        res.status(401);
-        res.send("User validation failed. Please check your data")
-    }
-})
+  try {
+    const user = new User(req.body);
+    console.log("User: ", user);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(401);
+    res.send("User validation failed. Please check your data");
+  }
+});
 
 router.get("/", async (req, res) => {
-    try {
-        const users = await User.find()
-        res.json(users)
-    } catch (error) {
-        res.send(error.message)
-    }
-})
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.send(error.message);
+  }
+});
 
-module.exports = router
+// router.get("/test", async (req, res) => {
+//   try {
+//     res.json({});
+//   } catch (error) {
+//     res.send(error.message);
+//   }
+// });
+
+module.exports = router;
